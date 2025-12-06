4ff0c9b3216a2135db8d769f445a6b059e117f90
indra <indra1nkuss@gmail.com>
2025-12-06 11:24:02 +0700

    feat(splashscreen2): menambahkan pengendali animasi dan struktur dasar initState-dispose

--- lib/splashsreen/splashscreen2.dart
+++ lib/splashsreen/splashscreen2.dart
@@ -7,7 +7,70 @@ class SplashScreen2 extends StatefulWidget {
   State<SplashScreen2> createState() => _SplashScreen2State();
 }
 
-class _SplashScreen2State extends State<SplashScreen2> {
+class _SplashScreen2State extends State<SplashScreen2>
+with TickerProviderStateMixin {
+  late AnimationController _fadeController;
+  late AnimationController _scaleController;
+  late AnimationController _rotateController;
+  late AnimationController _slideController;
+
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fadeController = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    );
+
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _rotateController = AnimationController(
+      duration: const Duration(seconds: 25),
+      vsync: this,
+    )..repeat();
+
+    _slideController = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _fadeController, curve: Curves.easeIn),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _scaleController, curve: Curves.elasticOut),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: _slideController, curve: Curves.easeOut),
+    );
+
+    _fadeController.forward();
+    _scaleController.forward();
+    _slideController.forward();
+  }
+
+  @override
+  void dispose() {
+    _fadeController.dispose();
+    _scaleController.dispose();
+    _rotateController.dispose();
+    _slideController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
