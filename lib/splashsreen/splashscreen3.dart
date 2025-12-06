cc5f09398776b5ac43a45745ffd6e163f37fb335
indra <indra1nkuss@gmail.com>
2025-12-06 17:01:23 +0700

    feat(splashscreen3):menambahkan pengontrol animasi dan metode siklus hidup ke SplashScreen3

--- lib/splashsreen/splashscreen3.dart
+++ lib/splashsreen/splashscreen3.dart
@@ -7,6 +7,47 @@ class SplashScreen3 extends StatefulWidget {
   State<SplashScreen3> createState() => _SplashScreen3State();
 }
 
+class _SplashScreen3State extends State<SplashScreen3> with TickerProviderStateMixin {
+  late AnimationController _fadeController;
+  late AnimationController _slideController;
+  late AnimationController _rotateController;
+  late AnimationController _particlesController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fadeController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..forward();
+
+    _slideController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..forward();
+
+    _rotateController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    )..repeat();
+
+    _particlesController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 15),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _fadeController.dispose();
+    _slideController.dispose();
+    _rotateController.dispose();
+    _particlesController.dispose();
+    super.dispose();
+  }
+
+
 class _SplashScreen3State extends State<SplashScreen3> {
   @override
   Widget build(BuildContext context) {
