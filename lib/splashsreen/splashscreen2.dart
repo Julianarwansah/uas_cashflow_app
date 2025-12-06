905a8175030f5b3aea3454e37889eaa2067f8083
indra <indra1nkuss@gmail.com>
2025-12-06 11:20:37 +0700

    feat(splashscreen2):create basic SplashScreen2 stateful widget with placeholder UI

---
+++ lib/splashsreen/splashscreen2.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  State<SplashScreen2> createState() => _SplashScreen2State();
+}
+
+class _SplashScreen2State extends State<SplashScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF0F172A),
+      body: Center(
+        child: Text(
+          "Splash Screen 2",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
