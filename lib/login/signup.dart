b42267a70cbc7ae3e6ee1bb2a493d914b2225b32
NamaAnda <julianarwansahh@gmail.com>
2025-12-07 16:21:02 +0700

    Menambahkan initState untuk setup animation controllers

--- lib/login/signup.dart
+++ lib/login/signup.dart
@@ -28,6 +28,38 @@ class _SignupScreenState extends State<SignupScreen>
   late Animation<double> _scaleAnimation;
 
   @override
+  void initState() {
+    super.initState();
+
+    _fadeController = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    );
+
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _rotateController = AnimationController(
+      duration: const Duration(seconds: 30),
+      vsync: this,
+    )..repeat();
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _fadeController, curve: Curves.easeIn));
+
+    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
+      CurvedAnimation(parent: _scaleController, curve: Curves.elasticOut),
+    );
+
+    _fadeController.forward();
+    _scaleController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(child: const Center(child: Text('Signup Screen'))),
