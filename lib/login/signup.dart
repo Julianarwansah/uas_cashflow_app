662fa906222033905b3767c1e5cb56f268ed10df
NamaAnda <julianarwansahh@gmail.com>
2025-12-07 16:07:17 +0700

    Menambahkan import dependencies untuk lottie dan firebase

--- lib/login/signup.dart
+++ lib/login/signup.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:math' as math;
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
