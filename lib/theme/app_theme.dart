d14a60a068ebaf9c55598788f5fae1f4ae0fba70
NamaAnda <julianarwansahh@gmail.com>
2025-12-26 13:33:58 +0700

    menambahkan file app_theme untuk pengaturan warna aplikasi

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static const Color primaryBlue = Color(0xFF1E3A8A);
+  static const Color accentBlue = Color(0xFF3B82F6);
+  static const Color lightBlue = Color(0xFF60A5FA);
+  static const Color softBlue = Color(0xFFDBEAFE);
+  static const Color darkBlue = Color(0xFF1E293B);
+
+  static const Color glassBackground = Color(0x1A3B82F6);
+  static const Color cardBackground = Color(0xFFFFFFFF);
+  static const Color scaffoldBackground = Color(0xFFF0F9FF);
+
+  static const Color incomeGreen = Color(0xFF10B981);
+  static const Color incomeGreenLight = Color(0xFFD1FAE5);
+  static const Color expenseRed = Color(0xFFEF4444);
+  static const Color expenseRedLight = Color(0xFFFEE2E2);
+
+  static const Color textPrimary = Color(0xFF1E293B);
+  static const Color textSecondary = Color(0xFF64748B);
+  static const Color textWhite = Color(0xFFFFFFFF);
+}
