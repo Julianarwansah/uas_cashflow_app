df28721e2c68f99455845339e25ac2e66d630a6f
NamaAnda <julianarwansahh@gmail.com>
2025-12-26 15:11:25 +0700

    feat(theme): Add label and amount text styles

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -97,4 +97,28 @@ class AppTheme {
     fontWeight: FontWeight.normal,
     color: textSecondary,
   );
+
+  static TextStyle get labelLarge => GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: textPrimary,
+  );
+
+  static TextStyle get labelMedium => GoogleFonts.inter(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: textSecondary,
+  );
+
+  static TextStyle get amountLarge => GoogleFonts.poppins(
+    fontSize: 36,
+    fontWeight: FontWeight.bold,
+    color: textWhite,
+  );
+
+  static TextStyle get amountMedium => GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: textPrimary,
+  );
 }
