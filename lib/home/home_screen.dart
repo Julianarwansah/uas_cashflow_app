853e7cb2d83d0300382dc09f622e8e441cef685a
NamaAnda <julianarwansahh@gmail.com>
2025-12-27 18:51:30 +0700

    siapin tempat buat nyimpen data stream transaksi

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -15,3 +15,7 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Stream<List<Transaction>> _transactionsStream;
+}
