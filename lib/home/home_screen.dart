2b6c8367f6dd10dbda388fd001437eea29bf5345
NamaAnda <julianarwansahh@gmail.com>
2025-12-27 18:50:58 +0700

    bikin widget home screen pake stateful

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -6,3 +6,12 @@ import '../theme/app_theme.dart';
 import '../models/transaction.dart';
 import '../cashflow/transaction_form_screen.dart';
 import '../notifications/notification_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  final VoidCallback? onViewAllTap;
+
+  const HomeScreen({super.key, this.onViewAllTap});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
