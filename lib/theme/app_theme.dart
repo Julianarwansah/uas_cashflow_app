1e6c1eec75b642e5701836bf676239669739ff12
NamaAnda <julianarwansahh@gmail.com>
2025-12-27 17:11:34 +0700

    masukin warna background kaca

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -7,4 +7,8 @@ class AppTheme {
   static const Color lightBlue = Color(0xFF60A5FA);
   static const Color softBlue = Color(0xFFDBEAFE);
   static const Color darkBlue = Color(0xFF1E293B);
+
+  static const Color glassBackground = Color(0x1A3B82F6);
+  static const Color cardBackground = Color(0xFFFFFFFF);
+  static const Color scaffoldBackground = Color(0xFFF0F9FF);
 }
