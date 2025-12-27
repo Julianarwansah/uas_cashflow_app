3d4d30f87c8c312f11906740774d223f641c55a5
NamaAnda <julianarwansahh@gmail.com>
2025-12-27 17:11:17 +0700

    nambahin warna biru utama

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AppTheme {}
+class AppTheme {
+  static const Color primaryBlue = Color(0xFF1E3A8A);
+  static const Color accentBlue = Color(0xFF3B82F6);
+  static const Color lightBlue = Color(0xFF60A5FA);
+  static const Color softBlue = Color(0xFFDBEAFE);
+  static const Color darkBlue = Color(0xFF1E293B);
+}
