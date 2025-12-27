4322583f74d59d9cbacf074cf20337a5a6c228bd
NamaAnda <julianarwansahh@gmail.com>
2025-12-27 21:05:28 +0700

    membuat widget

--- lib/cashflow/transaction_form_screen.dart
+++ lib/cashflow/transaction_form_screen.dart
@@ -7,3 +7,20 @@ import 'package:intl/intl.dart';
 import '../theme/app_theme.dart';
 import '../models/transaction.dart';
 import '../services/notification_service.dart';
+
+class TransactionFormScreen extends StatefulWidget {
+  final Transaction? transaction;
+  final VoidCallback? onSuccess;
+
+  const TransactionFormScreen({super.key, this.transaction, this.onSuccess});
+
+  @override
+  State<TransactionFormScreen> createState() => _TransactionFormScreenState();
+}
+
+class _TransactionFormScreenState extends State<TransactionFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
