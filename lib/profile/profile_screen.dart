dce5c74b7f599880b00a53a28e02d568790cbf75
NamaAnda <julianarwansahh@gmail.com>
2025-12-28 21:43:00 +0700

    menambahkan widget profile

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -3,3 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../theme/app_theme.dart';
 import '../notifications/notification_screen.dart';
 import 'edit_profile_screen.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
