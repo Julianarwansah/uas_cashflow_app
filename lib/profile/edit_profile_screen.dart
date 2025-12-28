c90323f9d53bbe8a9dc4a1ff30eaf6092d586b04
NamaAnda <julianarwansahh@gmail.com>
2025-12-28 22:56:09 +0700

    bikin file awal edit profile

---
+++ lib/profile/edit_profile_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import '../theme/app_theme.dart';
+
+class EditProfileScreen extends StatefulWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
