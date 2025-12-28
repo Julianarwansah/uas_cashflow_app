ef0fa29e14a0e200d31ed8be39b7ff20884d0ca3
NamaAnda <julianarwansahh@gmail.com>
2025-12-28 21:41:05 +0700

    menambahakn import yang akan di gunakan

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -1,1 +1,5 @@
-// Siapin lahan kosong buat profile screen baru
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../theme/app_theme.dart';
+import '../notifications/notification_screen.dart';
+import 'edit_profile_screen.dart';
