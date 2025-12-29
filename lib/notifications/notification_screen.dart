258465af2e5eb4ebe28e8880c42967ace3b43ff7
NamaAnda <julianarwansahh@gmail.com>
2025-12-29 09:50:50 +0700

    import sama class dasar

---
+++ lib/notifications/notification_screen.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../theme/app_theme.dart';
+import '../models/app_notification.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {}
