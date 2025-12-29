98c551e550bde1ac0aa11c600bd18a627375261e
NamaAnda <julianarwansahh@gmail.com>
2025-12-29 10:06:00 +0700

    import sama singleton

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
+
+class NotificationService {
+  static final NotificationService _instance = NotificationService._internal();
+  factory NotificationService() => _instance;
+  NotificationService._internal();
+}
