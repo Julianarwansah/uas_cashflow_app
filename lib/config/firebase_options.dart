86b83ea93d91f8434f87d1c4429b578b4c372ed8
NamaAnda <julianarwansahh@gmail.com>
2025-12-29 08:41:11 +0700

    add web configuration

--- lib/config/firebase_options.dart
+++ lib/config/firebase_options.dart
@@ -2,4 +2,13 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-class DefaultFirebaseOptions {}
+class DefaultFirebaseOptions {
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyD0kSCbFNwG9IHQV78aWLfre7xdTloFHi0',
+    appId: '1:712776728248:web:YOUR_WEB_APP_ID',
+    messagingSenderId: '712776728248',
+    projectId: 'uas-cashflow-app',
+    authDomain: 'uas-cashflow-app.firebaseapp.com',
+    storageBucket: 'uas-cashflow-app.firebasestorage.app',
+  );
+}
