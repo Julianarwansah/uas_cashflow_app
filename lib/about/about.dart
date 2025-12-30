fbdb0c5589fa4e797a5a6d0cbde9083905980a01
indra <indra1nkuss@gmail.com>
2025-12-31 00:08:45 +0700

    gaya: terapkan tema aplikasi dan warna latar

---
+++ lib/about/about.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile UI',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF3F8FE), // Background biru muda
+        primarySwatch: Colors.blue,
+        fontFamily: 'sans-serif',
+      ),
+      home: const ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
