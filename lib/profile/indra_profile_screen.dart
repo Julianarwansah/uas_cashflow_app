55dee99e142944c2560345268f7e10b4e9b5e320
indra <indra1nkuss@gmail.com>
2026-01-07 23:06:31 +0700

    init file indra_profile_screen.dart

---
+++ lib/profile/indra_profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+
+class IndraProfileScreen extends StatelessWidget {
+  const IndraProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
