e6284ea541e5ebee524d8713d459128f726b6489
NamaAnda <julianarwansahh@gmail.com>
2026-01-09 17:06:21 +0700

    memperbarui import untuk auth screens di main

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'config/firebase_options.dart';
-import 'auth/login.dart';
-import 'auth/signup.dart';
-import 'splash/splashscreen1.dart';
-import 'splash/splashscreen2.dart';
-import 'splash/splashscreen3.dart';
-import 'splash/splashscreen4.dart';
+import 'auth/login_screen.dart';
+import 'auth/register_screen.dart';
+import 'splash/splash_screen_1.dart';
+import 'splash/splash_screen_2.dart';
+import 'splash/splash_screen_3.dart';
+import 'splash/splash_screen_4.dart';
 import 'navigation/main_navigation.dart';
 import 'theme/app_theme.dart';
 import 'cashflow/transaction_form_screen.dart';
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
         '/splash3': (context) => const SplashScreen3(),
         '/splash4': (context) => const SplashScreen4(),
         '/login': (context) => const LoginScreen(),
-        '/signup': (context) => const SignupScreen(),
+        '/signup': (context) => const RegisterScreen(),
         '/home': (context) => const MainNavigation(),
         '/add-transaction': (context) => const TransactionFormScreen(),
       },
